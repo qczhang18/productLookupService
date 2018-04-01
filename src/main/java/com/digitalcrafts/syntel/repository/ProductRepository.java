3af64b9baa73a1ceb57ae8becb13400b304910cc
qczhang18 <qczhang18@gmail.com>
2018-03-31 22:14:05 -0400

    refactor to use JPA

---
+++ src/main/java/com/digitalcrafts/syntel/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.digitalcrafts.syntel.repository;
+
+import com.digitalcrafts.syntel.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
